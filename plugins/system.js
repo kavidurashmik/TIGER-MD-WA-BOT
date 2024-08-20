f61f13f85f4218cf853628090f2ef1d109b0f9ee
m.r kaviya <113000316+kavidurashmik@users.noreply.github.com>
2024-08-20 14:49:35 +0530

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -11,10 +11,10 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-let status = `*Uptime:*  ${runtime(process.uptime())}
-*Ram usage:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
-*HostName:* ${os.hostname()}
-*Owner:* 🖤💥KAVIYA OFFICIAL💥🖤
+let status = `*⌚ඔබගේ බොට් ක්‍රියා කරන කාලය⌚:*  ${runtime(process.uptime())}
+*💾YOURE PLATFORM MEMORY USAGE💾:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+*⭕YOURE PLATFORM NAME⭕:* ${os.hostname()}
+*🧑‍💻මෙම BOTහි අයිතිකරු🧑‍💻:* 🖤💥KAVIYA OFFICIAL💥🖤
 `
 return reply(`${status}`)
 
